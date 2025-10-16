f6c681bed515016e7c5cdb9319984c86b81b01fb
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-16 16:47:16 +0700

    refactor title

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
             <CardHeader>
               <CardTitle>Login to your account</CardTitle>
               <CardDescription>
-                Enter your email below to login to your account
+                Enter your username below to login to your account
               </CardDescription>
             </CardHeader>
             <CardContent>
