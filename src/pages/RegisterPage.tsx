5f90fab75227fa1e7dcac338e5805b36d79fc027
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-16 15:41:47 +0700

    create register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,10 +1,111 @@
-import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Input } from "@/components/ui/input";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import ButtonComponent from "@/components/ButtonComponent";
+import { authSchema, type AuthSchemaType } from "@/utils/loginSchema";
 
 export default function RegisterPage() {
+  const form = useForm<AuthSchemaType>({
+    resolver: zodResolver(authSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function submitRegister(values: AuthSchemaType) {
+    console.log(values);
+  }
   return (
-    <div>
-      {" "}
-      <Button>Register</Button>
+    <div className="min-w-screen min-h-screen flex justify-center items-center">
+      <Form {...form}>
+        <Card className="w-full max-w-sm">
+          <form onSubmit={form.handleSubmit(submitRegister)}>
+            <CardHeader>
+              <CardTitle>Register your account</CardTitle>
+              <CardDescription>
+                Enter your username and password below to register to your
+                account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2 pt-6">
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Input your username here..."
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Input your password here..."
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter className="flex-col gap-2 pt-6">
+              <ButtonComponent
+                type="submit"
+                text="Register"
+                className="w-full"
+              />
+
+              <div className="mt-4 text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </CardFooter>
+          </form>
+        </Card>
+      </Form>
     </div>
   );
 }
