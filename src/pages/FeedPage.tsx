50ec350362326f7e631225e821a5932c842d6bea
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-17 08:04:27 +0700

    implement interceptor

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -1,7 +1,19 @@
 import ButtonComponent from "@/components/ButtonComponent";
 import { http } from "@/utils/axios";
+import { useEffect } from "react";
 
 export default function FeedPage() {
+  useEffect(() => {
+    fetchPost();
+  }, []);
+  async function fetchPost() {
+    try {
+      const response = await http.get("/feed?page=1&limit=2");
+      console.log({ response });
+    } catch (error) {
+      console.log({ error });
+    }
+  }
   async function logout() {
     try {
       const response = await http.post("/logout");
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,20 @@
 import axios from "axios";
-
 export const http = axios.create({
   baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
+
+http.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      await http.post("/refresh-token");
+      console.log("refresh token applied");
+    }
+    console.log({ errorInterceptor: error });
+  }
+);
