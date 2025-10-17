2eee90ad8260e9e6464f39729853c2c05a7bafcf
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-17 16:05:18 +0700

    implement layout

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,19 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { Toaster } from "./components/ui/sonner";
 import FeedPage from "./pages/FeedPage";
+import PublicLayout from "./layouts/PublicLayout";
+import AuthLayout from "./layouts/AuthLayout";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/feed" element={<FeedPage />} />
+      <Route element={<PublicLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+      <Route element={<AuthLayout />}>
+        <Route path="/feed" element={<FeedPage />} />
+      </Route>
     </Routes>
     <Toaster />
   </BrowserRouter>
