3210b09aac32b6b91fa5e3ae262af744b6cd3214
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-17 17:43:14 +0700

    refactor function logout

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -1,24 +1,17 @@
 import ButtonComponent from "@/components/ButtonComponent";
 import useFetchPostPagination from "@/hooks/useDataPagination";
-import { http } from "@/utils/axios";
+import { logout } from "@/services/AuthService";
 import { useNavigate } from "react-router";
-import { toast } from "sonner";
 
 export default function FeedPage() {
   const navigate = useNavigate();
   const { post, setPage } = useFetchPostPagination();
 
-  async function logout() {
-    try {
-      const response = await http.get("/logout");
-      console.log({ response });
-      if (response.status === 200) {
-        navigate("/login");
-        toast.success("Bye bye, come back later!");
-      }
-    } catch (error) {
-      console.log({ error });
-    }
-  }
-  return <ButtonComponent type="button" handleClick={logout} text="Logout" />;
+  return (
+    <ButtonComponent
+      type="button"
+      handleClick={() => logout(navigate)}
+      text="Logout"
+    />
+  );
 }
